2452f3d45e0fdaa7c469fd3ede6eed5c0804adb3
harshrajput1506 <harshrajput2906@gmail.com>
2025-09-26 20:52:06 +0530

    refactor: simplify authentication flow by removing unused login request method and directly using Firebase user model

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:hand_cricket/core/contstants/secure_constants.dart';
 import 'package:hand_cricket/core/failures/failures.dart';
 import 'package:hand_cricket/models/user_model.dart';
 import 'package:logger/logger.dart';
@@ -11,7 +10,6 @@ class AuthService {
   final FirebaseAuth _auth;
   final GoogleSignIn _googleSignIn;
   final FirebaseFirestore _firestore;
-  final Dio _dio;
   final Logger _logger = Logger();
 
   AuthService({
@@ -21,8 +19,7 @@ class AuthService {
     required Dio dio,
   }) : _auth = auth,
        _googleSignIn = googleSignIn,
-       _firestore = firestore,
-       _dio = dio;
+       _firestore = firestore;
 
   // Sign in whith google
   Future<UserModel?> signInWithGoogle() async {
@@ -56,19 +53,7 @@ class AuthService {
         throw AuthFailure('User is null after Google sign-in');
       }
 
-      final data = await _loginRequest(
-        uid: user.uid,
-        email: user.email,
-        name: user.displayName,
-        profilePictureUrl: user.photoURL,
-      );
-
-      if (data.isEmpty) {
-        _logger.e('User data is empty after login request');
-        throw AuthFailure('User data is empty after login request');
-      }
-
-      final userModel = UserModel.fromJson(data);
+      final userModel = UserModel.fromFirebaseUser(user);
       _logger.d('User signed in with Google: $userModel');
       return userModel;
     } catch (e) {
@@ -88,17 +73,8 @@ class AuthService {
       }
 
       final user = userCredential.user!;
-      final data = await _loginRequest(
-        uid: user.uid,
-        email: user.email,
-        name: user.displayName,
-        profilePictureUrl: user.photoURL,
-      );
-      if (data.isEmpty) {
-        _logger.e('User data is empty after anonymous login request');
-        throw AuthFailure('User data is empty after anonymous login request');
-      }
-      final userModel = UserModel.fromJson(data);
+
+      final userModel = UserModel.fromFirebaseUser(user);
       _logger.d('User signed in anonymously: $userModel');
       return userModel;
     } catch (e) {
@@ -134,60 +110,6 @@ class AuthService {
     } catch (e) {
       _logger.e('Error getting current user: $e');
       throw Exception('Failed to get current user from firebase');
-    }
-  }
-
-  Future<Map<String, dynamic>> _loginRequest({
-    required String uid,
-    String? email,
-    String? name,
-    String? profilePictureUrl,
-  }) async {
-    try {
-      final baseUrl = SecureConstants.baseUrl;
-
-      final response = await _dio.post(
-        '$baseUrl/users/login',
-        data: {
-          'uid': uid,
-          'email': email,
-          'name': name,
-          'profilePictureUrl': profilePictureUrl,
-        },
-      );
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        _logger.d('Login request successful: ${response.data}');
-        if (response.data is! Map<String, dynamic> &&
-            response.data['success'] != true) {
-          _logger.e(
-            'Response data is not in expected format: ${response.data} ',
-          );
-          throw ServerFailure('Invalid response format');
-        }
-        // check if 'user' key exists in response data
-        if (!response.data.containsKey('user')) {
-          _logger.e(
-            'Response data does not contain "user" key: ${response.data}',
-          );
-          throw ServerFailure('User data not found in response');
-        }
-        return response.data['user'] as Map<String, dynamic>;
-      } else {
-        _logger.e('Login request failed with status: ${response.statusCode}');
-        throw ServerFailure('Failed to log in user');
-      }
-    } on DioException catch (e) {
-      _logger.e('DioException during login request: $e');
-      if (e.type == DioExceptionType.connectionTimeout ||
-          e.type == DioExceptionType.sendTimeout ||
-          e.type == DioExceptionType.receiveTimeout) {
-        throw NetworkFailure('Network error occurred during login');
-      } else {
-        throw ServerFailure('Failed to log in user');
-      }
-    } catch (e) {
-      _logger.e('Error logging in user: $e');
-      throw ServerFailure('Failed to log in user');
     }
   }
 }
