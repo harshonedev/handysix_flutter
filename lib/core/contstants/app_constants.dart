df8728d095277908c63553e3ab75bd2438a2b5ae
harshrajput1506 <harshrajput2906@gmail.com>
2025-10-26 23:21:33 +0530

    chore: update prisma client version to 6.18.0 in package.json
    fix(auth): ensure Firebase UID validation returns proper response
    
    feat(provider): add user remote and local services to providers
    
    feat(constants): add backend and API base URLs to app constants
    
    feat(failures): add ConnectionFailure class for better error handling
    
    feat(user_model): extend UserModel to include uid and stats, add StatsModel
    
    refactor(auth_service): integrate user remote and local services for user management
    
    fix(plugin): register shared_preferences plugin in GeneratedPluginRegistrant.swift
    
    chore(deps): update shared_preferences dependency in pubspec.yaml and pubspec.lock
    
    feat(user_local_service): implement local user storage using SharedPreferences
    
    feat(user_remote_service): create remote service for user login and retrieval

--- lib/core/contstants/app_constants.dart
+++
@@ -1,7 +0,0 @@
-class AppConstants {
-  static const String avatarUrl =
-      'https://res.cloudinary.com/drhyzkn62/image/upload/v1746772368/avatar-avocado-food-svgrepo-com_fenooq.png';
-
-  static const String computerAvatarUrl =
-      'https://res.cloudinary.com/drhyzkn62/image/upload/v1751439753/avatar-einstein-professor-svgrepo-com_ni5raa.png'; // Replace with actual URL or path
-}
